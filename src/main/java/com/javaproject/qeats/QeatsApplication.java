2e8acde8744cc895767a7d7275267fb7ef55754b
shakti <shaktisarswat01@gmail.com>
2024-02-13 22:11:23 +0530

    Initial Commit

---
+++ src/main/java/com/javaproject/qeats/QeatsApplication.java
@@ -0,0 +1,14 @@
+package com.javaproject.qeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QeatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QeatsApplication.class, args);
+		System.out.println("Application Started");
+	}
+
+}
---
+++ src/test/java/com/javaproject/qeats/QeatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.qeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QeatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
