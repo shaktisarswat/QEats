3b019895d9976017a009937d1040ecd079c0c74e
shakti <shaktisarswat01@gmail.com>
2024-05-12 13:39:32 +0530

    formated Redis configuration

--- src/main/java/com/javaproject/qeats/configs/RedisConfiguration.java
+++ src/main/java/com/javaproject/qeats/configs/RedisConfiguration.java
@@ -1,4 +1,3 @@
-
 package com.javaproject.qeats.configs;
 
 import jakarta.annotation.PostConstruct;
@@ -14,99 +13,99 @@ import java.time.Duration;
 @Component
 public class RedisConfiguration {
 
-  public static final String redisHost = "localhost";
+    public static final String redisHost = "localhost";
 
-  public static final int REDIS_ENTRY_EXPIRY_IN_SECONDS = 3600;
+    public static final int REDIS_ENTRY_EXPIRY_IN_SECONDS = 3600;
 
-  public static final String EXCHANGE_NAME = "rabbitmq-exchange";
-  public static final String QUEUE_NAME = "rabbitmq-queue";
-  public static final String ROUTING_KEY = "qeats.postorder";
-
-
-  private int redisPort;
-  private JedisPool jedisPool;
+    public static final String EXCHANGE_NAME = "rabbitmq-exchange";
+    public static final String QUEUE_NAME = "rabbitmq-queue";
+    public static final String ROUTING_KEY = "qeats.postorder";
 
 
-  @Value("${spring.redis.port}")
-  public void setRedisPort(int port) {
-    System.out.println("setting up redis port to " + port);
-    redisPort = port;
-  }
+    private int redisPort;
+    private JedisPool jedisPool;
 
-  private static JedisPoolConfig buildPoolConfig() {
-    final JedisPoolConfig poolConfig = new JedisPoolConfig();
-    poolConfig.setMaxTotal(128);
-    poolConfig.setMaxIdle(128);
-    poolConfig.setMinIdle(16);
-    poolConfig.setTestOnBorrow(true);
-    poolConfig.setTestOnReturn(true);
-    poolConfig.setTestWhileIdle(true);
-    poolConfig.setMinEvictableIdleTimeMillis(Duration.ofSeconds(60).toMillis());
-    poolConfig.setTimeBetweenEvictionRunsMillis(Duration.ofSeconds(30).toMillis());
-    poolConfig.setNumTestsPerEvictionRun(3);
-    poolConfig.setBlockWhenExhausted(true);
-    return poolConfig;
-  }
-
-  /**
-   * Initializes the cache to be used in the code. TIP: Look in the direction of `JedisPool`.
-   */
-  @PostConstruct
-  public void initCache() {
-    final JedisPoolConfig poolConfig = buildPoolConfig();
-    try {
-      jedisPool = new JedisPool(poolConfig, redisHost, redisPort);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-  public JedisPool getJedisPool() {
-    if (jedisPool != null) {
-      return jedisPool;
+    private static JedisPoolConfig buildPoolConfig() {
+        final JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(128);
+        poolConfig.setMaxIdle(128);
+        poolConfig.setMinIdle(16);
+        poolConfig.setTestOnBorrow(true);
+        poolConfig.setTestOnReturn(true);
+        poolConfig.setTestWhileIdle(true);
+        poolConfig.setMinEvictableIdleTimeMillis(Duration.ofSeconds(60).toMillis());
+        poolConfig.setTimeBetweenEvictionRunsMillis(Duration.ofSeconds(30).toMillis());
+        poolConfig.setNumTestsPerEvictionRun(3);
+        poolConfig.setBlockWhenExhausted(true);
+        return poolConfig;
     }
 
-    try {
-      final JedisPoolConfig poolConfig = buildPoolConfig();
-      jedisPool = new JedisPool(poolConfig, redisHost, redisPort);
-    } catch (Exception e) {
-      // We don't want to do anything for if cache initialization fails.
-      e.printStackTrace();
+    @Value("${spring.redis.port}")
+    public void setRedisPort(int port) {
+        System.out.println("setting up redis port to " + port);
+        redisPort = port;
     }
 
-    return jedisPool;
-  }
-
-
-  /**
-   * Checks is cache is intiailized and available.
-   * TIP: This would generally mean checking via {@link JedisPool}
-   * @return true / false if cache is available or not.
-   */
-  public boolean isCacheAvailable() {
-
-    if (jedisPool == null) {
-      return false;
+    /**
+     * Initializes the cache to be used in the code. TIP: Look in the direction of `JedisPool`.
+     */
+    @PostConstruct
+    public void initCache() {
+        final JedisPoolConfig poolConfig = buildPoolConfig();
+        try {
+            jedisPool = new JedisPool(poolConfig, redisHost, redisPort);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    try (Jedis jedis = this.getJedisPool().getResource()) {
-      return true;
-    } catch (Exception e) {
-      return false;
-    }
-  }
+    public JedisPool getJedisPool() {
+        if (jedisPool != null) {
+            return jedisPool;
+        }
 
-  /**
-   * Destroy the cache. TIP: This is useful if cache is stale or while performing tests.
-   */
-  public void destroyCache() {
-    if (jedisPool != null) {
-      jedisPool.getResource().flushAll();
-      jedisPool.destroy();
-      jedisPool = null;
+        try {
+            final JedisPoolConfig poolConfig = buildPoolConfig();
+            jedisPool = new JedisPool(poolConfig, redisHost, redisPort);
+        } catch (Exception e) {
+            // We don't want to do anything for if cache initialization fails.
+            e.printStackTrace();
+        }
+
+        return jedisPool;
     }
 
-  }
+
+    /**
+     * Checks is cache is intiailized and available.
+     * TIP: This would generally mean checking via {@link JedisPool}
+     *
+     * @return true / false if cache is available or not.
+     */
+    public boolean isCacheAvailable() {
+
+        if (jedisPool == null) {
+            return false;
+        }
+
+        try (Jedis jedis = this.getJedisPool().getResource()) {
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /**
+     * Destroy the cache. TIP: This is useful if cache is stale or while performing tests.
+     */
+    public void destroyCache() {
+        if (jedisPool != null) {
+            jedisPool.getResource().flushAll();
+            jedisPool.destroy();
+            jedisPool = null;
+        }
+
+    }
 
 }
 
